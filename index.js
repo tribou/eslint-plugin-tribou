443237555ef949a606dbd48c184d444b832472cd
Aaron Tribou <aaron.tribou@gmail.com>
2017-05-31 18:30:15 -0400

    Fixed ability to use extends: plugin:tribou/recommended Adjusted rules to match eslint-config-tribou@^3 Embedded ESLint instance for future use

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var ESLint = require('eslint')
 var Merge = require('deepmerge')
 var AirbnbConfig = require('eslint-config-airbnb')
 var StandardConfig = require('eslint-config-standard')
@@ -21,75 +22,73 @@ var merged = Merge.all([
 
 module.exports = {
 
+  eslint: ESLint,
+
   rules: merged,
 
   configs: {
     airbnb: AirbnbConfig,
     standard: StandardConfig,
 
-    tribou: {
-      recommended: {
-        extends: [
-          'eslint-config-airbnb',
-          'eslint-config-standard',
-        ],
+    recommended: {
 
-        parser: 'babel-eslint',
+      extends: [
+        'eslint-config-airbnb',
+        'eslint-config-standard',
+        'plugin:flowtype/recommended',
+      ],
 
-        plugins: [
-          'flowtype',
-        ],
+      parser: 'babel-eslint',
 
-        rules: {
+      plugins: [
+        'flowtype',
+      ],
 
-          'flowtype/define-flow-type': 1,
-          'flowtype/use-flow-type': 1,
-          'flowtype/require-parameter-type': ['error', 'always', {
-            excludeArrowFunctions: 'expressionsOnly',
-          }],
-          'flowtype/require-return-type': ['error', 'always', {
-            excludeArrowFunctions: 'expressionsOnly',
-          }],
-          'flowtype/require-valid-file-annotation': ['error', 'always'],
-          'flowtype/valid-syntax': ['error', 'always'],
+      rules: {
 
-          'import/no-extraneous-dependencies': ['error', {
-            devDependencies: true,
-          }],
+        'import/no-extraneous-dependencies': ['error', {
+          devDependencies: true,
+        }],
+        'import/no-duplicates': ['error', 'always'],
+        'no-duplicate-imports': 0, // handled by import/no-duplicates
 
-          'quote-props': ['error', 'as-needed', { numbers: true }],
+        'quote-props': ['error', 'as-needed', { numbers: true }],
 
-          'react/jsx-filename-extension': [0],
-          'react/require-extension': [0],
+        'react/jsx-filename-extension': [0],
+        'react/require-extension': [0],
 
-          strict: 0,
-          'arrow-body-style': [2, 'always'],
-          'arrow-parens': [2, 'always'],
-          'brace-style': [2, 'stroustrup'],
-          'comma-dangle': [2, 'always-multiline'],
-          'max-len': [2, {
-            code: 80,
-            tabWidth: 2,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-            ignoreUrls: true,
-          }],
-          'operator-linebreak': [2, 'before'],
-          'no-multiple-empty-lines': [2, {
-            max: 2,
-            maxEOF: 0,
-          }],
-          'no-underscore-dangle': [0],
-          'padded-blocks': [2, 'always'],
+        strict: 0,
+        'arrow-body-style': [2, 'always'],
+        'arrow-parens': [2, 'always'],
+        'brace-style': [2, 'stroustrup'],
+        'comma-dangle': [2, 'always-multiline'],
+        'max-len': [2, {
+          code: 80,
+          tabWidth: 2,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreUrls: true,
+        }],
+        'operator-linebreak': [2, 'before'],
+        'no-multiple-empty-lines': [2, {
+          max: 2,
+          maxEOF: 0,
+        }],
+        'no-underscore-dangle': [0],
+        'padded-blocks': [2, 'always'],
 
-        },
+      },
 
-        settings: {
-          flowtype: {
-            onlyFilesWithFlowAnnotation: false,
-          },
+      settings: {
+        flowtype: {
+          onlyFilesWithFlowAnnotation: false,
         },
       },
+
+      env: {
+        jest: true,
+      }
+
     },
   },
 
